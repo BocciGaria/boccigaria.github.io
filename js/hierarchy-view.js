1d7e2e9599d046764fbc4de4a9ae0908152e4f89
BocciGaria <s.otsuka0726@gmail.com>
2023-11-29 22:24:56 +0900

    背景の左右画像に動きを追加

--- js/hierarchy-view.js
+++
@@ -1,6 +0,0 @@
-const simpleCards = document.getElementsByClassName("hv-toggle-card");
-Array.from(simpleCards).forEach((element) => {
-    element.addEventListener("mouseover", () => {
-        element.classList.toggle("hv-hidden-card");
-    });
-});
